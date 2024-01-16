23f8116a2fac72e0f5bc1afb29ccdaa6a23a8717
david <152167851+David-CEP@users.noreply.github.com>
2024-01-16 10:02:33 +0100

    end scene + enemies

--- Assets/bala.cs
+++ Assets/bala.cs
@@ -1,28 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class bala : MonoBehaviour
 {
     public GameObject jugador;
-    public ParticleSystem muerteEnemigo;
+    public float bulletVel = 4f;
+    public int enemyCounter = 0;
+    public float checker = 0;
+
+    private void Update()
+    {
+        if(gameObject.transform.position.y > 5.15f)
+        {
+            gameObject.SetActive(false);
+            gameObject.transform.position = new Vector2(jugador.transform.position.x, jugador.transform.position.y);
+            gameObject.SetActive(true);
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.up * bulletVel;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Respawn")
+        if (collision.gameObject.tag == "Player")
         {
-            gameObject.SetActive(false);
-            gameObject.transform.position = new Vector2(jugador.transform.position.x, jugador.transform.position.y);
-            gameObject.SetActive(true);
-            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.up * 4f;
-        }else if(collision.gameObject.tag == "Player")
-        {
-            muerteEnemigo.Play();
-            collision.gameObject.SetActive(false);
-            gameObject.SetActive(false);
-            gameObject.transform.position = new Vector2(jugador.transform.position.x, jugador.transform.position.y);
-            gameObject.SetActive(true);
-            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.up * 4f;
+            enemyCounter++;
+            if (enemyCounter == 12)
+            {
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                bulletVel += bulletVel * 0.1f;
+                collision.gameObject.SetActive(false);
+                gameObject.SetActive(false);
+                gameObject.transform.position = new Vector2(jugador.transform.position.x, jugador.transform.position.y);
+                gameObject.SetActive(true);
+                gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.up * bulletVel;
+            }
         }
     }
 }
--- Assets/jugador.cs
+++ Assets/jugador.cs
@@ -1,33 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class jugador : MonoBehaviour
 {
     public GameObject bala;
     public bool firstBullet = false;
+    public float bulletVel = 4f;
+
+    void Start()
+    {
+        
+        
+        GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject enemigo in enemigos)
+        {
+            enemigo.GetComponent<Rigidbody2D>().velocity = Vector2.down * 1f;
+        }
+    }
 
     void Update()
     {
         Vector2 position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.1f);
         Vector2 altPos = new Vector2(-2.62f, position.y);
         Vector2 altPosDos = new Vector2(2.60f, position.y);
-        if(position.x < -2.62f)
+        if (position.x < -2.62f)
         {
             GetComponent<Rigidbody2D>().MovePosition(altPos);
-        }else if(position.x > 2.60f)
+        }
+        else if (position.x > 2.60f)
         {
             GetComponent<Rigidbody2D>().MovePosition(altPosDos);
-        }else
+        }
+        else
         {
             GetComponent<Rigidbody2D>().MovePosition(position);
         }
 
-        if(!firstBullet){
+        if (!firstBullet)
+        {
             bala.SetActive(false);
             bala.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
             bala.SetActive(true);
-            bala.GetComponent<Rigidbody2D>().velocity = Vector2.up * 4f;
+            bala.GetComponent<Rigidbody2D>().velocity = Vector2.up * bulletVel;
             firstBullet = true;
         }
     }
